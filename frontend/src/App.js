ae1e9bd493f367b52393fde46214155fa4eb8e1e
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2023-02-19 02:26:13 +0530

    Designed Navbar and Icon component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
-import "./App.css";
+import './App.css'
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/"></Route>
-        <Route exact path="/about"></Route>
-        <Route exact path="/contact"></Route>
-      </Routes>
-      <Footer />
+      <StyledEngineProvider injectFirst>
+        <Header />
+        <Routes>
+          <Route exact path="/"></Route>
+          <Route exact path="/about"></Route>
+          <Route exact path="/contact"></Route>
+        </Routes>
+        <Footer />
+      </StyledEngineProvider>
     </Router>
   );
 }
