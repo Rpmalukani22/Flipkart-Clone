4aa822a2cbc5566339953f321dbb291e26bb1dc3
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2023-02-19 20:20:28 +0530

    optimization

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,48 +1,44 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 import Search from "./Search/Search";
 
 export default function Header() {
   return (
-    <Box
-      position="sticky"
-      className={styles["header-box"]}
-    >
-      <AppBar className={styles["appbar"]}>
-        <Toolbar className={styles["toolbar"]}>
-          {/*----------------------------------- Flipkart Navbar Logo ---------------------------------------------------- */}
-          <Box className={styles["logo-box"]}>
-            <img
-              src="./flipkart-text-logo.png"
-              className={styles["navbar-icon"]}
-              alt="Flipkart Logo"
-            />
-            <Typography
-              className={styles["icon-sublink"]}
-              variant="caption"
-              component="div"
-            >
-              <a href="http://www.google.com">
-                Explore{" "}
-                <span>
-                  Plus <img src="./flipkart-plus-logo.png"></img>
-                </span>
-              </a>
-            </Typography>
-          </Box>
+    <AppBar position="sticky" className={styles["appbar"]}>
+      <Toolbar className={styles["toolbar"]}>
+        {/*----------------------------------- Flipkart Navbar Logo ---------------------------------------------------- */}
+        <Box className={styles["logo-box"]}>
+          <img
+            src="./flipkart-text-logo.png"
+            className={styles["navbar-icon"]}
+            alt="Flipkart Logo"
+          />
+          <Typography
+            className={styles["icon-sublink"]}
+            variant="caption"
+            component="div"
+          >
+            <a href="http://www.google.com">
+              Explore{" "}
+              <span>
+                Plus <img src="./flipkart-plus-logo.png"></img>
+              </span>
+            </a>
+          </Typography>
+        </Box>
 
-          {/*---------------------------------------------Navbar Searchbar---------------------------------------------*/}
+        {/*---------------------------------------------Navbar Searchbar---------------------------------------------*/}
 
-          <Search />
+        <Search />
 
-          {/*---------------------------------------------Navbar Links---------------------------------------------*/}
+        {/*---------------------------------------------Navbar Links---------------------------------------------*/}
 
-          <Box className={styles["nav-links"]}>
-            <Button className={styles["login-button"]}>Login</Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+        <Box className={styles["nav-links"]}>
+          <Button className={styles["login-button"]}>Login</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
