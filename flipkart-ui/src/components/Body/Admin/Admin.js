1ecfe73f24ff72e56eb7212a0289869ae0edb0cd
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2023-05-05 00:33:15 +0530

    Refactoring

--- flipkart-ui/src/components/Body/Admin/Admin.js
+++ flipkart-ui/src/components/Body/Admin/Admin.js
@@ -5,17 +5,51 @@
  * -----
  * Copyright (c) 2023 Ruchitesh Malukani
  */
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Admin.css";
 import "rapidoc";
 import { urlService } from "../../../services/urls";
+import BlockIcon from "@mui/icons-material/Block";
 import { useAuth } from "react-oidc-context";
+import useGetData from "../../../hooks/useGetData";
+import { Box, Typography } from "@mui/material";
 
 export default function Admin() {
   const auth = useAuth();
+  const spec = useGetData(urlService.getApiSpecs());
+  const specRef = useRef();
+  useEffect(() => {
+    try {
+      specRef.current.loadSpec(spec);
+    } catch (e) {
+      console.log("error", e);
+    }
+    console.log(specRef);
+  }, [spec]);
+  if (auth.isAuthenticated === false || (!spec)) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "90vh",
+        }}
+      >
+        <Typography
+          sx={{ color: "#878787", display: "flex", alignItems: "center" }}
+          variant="h2"
+        >
+          <BlockIcon fontSize="1rem" sx={{ m: 2 }} /> Access Denied. You have
+          insufficient privileges.
+        </Typography>
+      </Box>
+    );
+  }
   return (
     <rapi-doc
-      spec-url={urlService.getApiSpecs()}
+      ref={specRef}
+      loadSpec={() => spec}
       render-style="focused"
       show-header="false"
       show-info="false"
